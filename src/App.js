8e235953875375d174f7e018721161eabf703845
Mayank Mundra <may.c.lnw1432@gmail.com>
2023-07-14 13:05:38 +0530

    updated structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Bookings from './components/Bookings';
+import ConfirmedBookings from './components/ConfirmedBookings';
+import Footer from './components/Footer';
+import Headers from './components/Headers';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Headers />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/bookings" element={<Bookings />} />
+        <Route path="/confirmed-bookings" element={<ConfirmedBookings />} />
+      </Routes>
+
+      <Footer />
+    </>
   );
 }
 
