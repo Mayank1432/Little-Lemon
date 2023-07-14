5450abdd987b5edb924390248e2531287c49f591
Mayank Mundra <may.c.lnw1432@gmail.com>
2023-07-14 14:23:48 +0530

    Update pages.js

--- src/utils/pages.js
+++ src/utils/pages.js
@@ -1,6 +1,6 @@
 
 const pages = new Map();
-pages.set('home', { name: 'Home', path: '/', anchorable: true });
+pages.set('home', { name: 'Home', path: '/Little-Lemon', anchorable: true });
 pages.set('about', { name: 'About', path: '/about', anchorable: true });
 pages.set('menu', { name: 'Menu', path: '/menu', anchorable: true });
 pages.set('bookings', { 
